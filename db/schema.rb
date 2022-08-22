508d279b3aefed72bd9669935cf173b4cc388630
Pauline Bewerunge <bewerungepauline@gmail.com>
2022-08-22 18:04:00 +0200

    booking, granny_offers, user Models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_142227) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_160253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "status"
+    t.bigint "user_id", null: false
+    t.bigint "granny_offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["granny_offer_id"], name: "index_bookings_on_granny_offer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "granny_offers", force: :cascade do |t|
+    t.integer "hug_amount"
+    t.boolean "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_granny_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +43,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_142227) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.integer "age"
+    t.boolean "granny"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "granny_offers"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "granny_offers", "users"
 end

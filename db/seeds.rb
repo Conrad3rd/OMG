5a5ee67412185d1ff813383a9206fa72155ddab8
Conrad Esche <conrad.3rd@gmail.com>
2022-08-23 16:56:58 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ puts 'Cleaning database users...'
 # Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 User.create(email: "oma@gmail.com", first_name: "Esther", last_name: "Wynder", address: "123 Fakestreet", age: 99, granny: true)
 
-
 # puts 'Cleaning database bookings...'
 # bookings.destroy_all
 

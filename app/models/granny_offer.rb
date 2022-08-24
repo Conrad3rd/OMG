5dcd13917246566daf14a4ae95d050b0cc97b30f
Pauline Bewerunge <bewerungepauline@gmail.com>
2022-08-24 15:19:12 +0200

    resolve conflict

--- app/models/granny_offer.rb
+++ app/models/granny_offer.rb
@@ -2,9 +2,5 @@ class GrannyOffer < ApplicationRecord
   belongs_to :user
 
   validates :hug_amount, presence: true
-<<<<<<< HEAD
   validates :details, presence: true
-=======
-  validates :available, presence: true
->>>>>>> master
 end

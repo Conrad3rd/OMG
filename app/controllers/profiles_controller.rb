06c9d227d8f16d32d33feac50843a9e0d99039f7
Antonios Papatheodorou <antoniospapatheodorou@gmail.com>
2022-08-26 13:08:10 +0200

    different layout according to checkout button grandma or not

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,12 +2,17 @@ class ProfilesController < ApplicationController
   def show
     @user = current_user
 
-    
+
   end
 
   def edit
     @user = current_user
   end
 
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:granny)
+  # end
 
 end
